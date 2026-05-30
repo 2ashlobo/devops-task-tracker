3d2a9a9109455a85b64bc11204ba13a0c4993474
Lobo <ashley.lobo@dell.com>
2026-05-30 19:54:13 +0530

    Change region back to us-east-1 for free tier eligibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
